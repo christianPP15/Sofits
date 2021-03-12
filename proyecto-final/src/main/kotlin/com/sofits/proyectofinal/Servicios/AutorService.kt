b45d93f15a707877ca7bbfb63206946d68d2fbee
ChristianPP15 <christianpayo32@gmail.com>
2021-03-12 14:13:09 +0100

    Servicios base

---
+++ proyecto-final/src/main/kotlin/com/sofits/proyectofinal/Servicios/AutorService.kt
@@ -0,0 +1,10 @@
+package com.sofits.proyectofinal.Servicios
+
+import com.sofits.proyectofinal.Modelos.Autor
+import com.sofits.proyectofinal.Modelos.AutorRepository
+import com.sofits.proyectofinal.Servicios.base.BaseService
+import java.util.*
+
+class AutorService : BaseService<Autor,UUID,AutorRepository>(){
+
+}
