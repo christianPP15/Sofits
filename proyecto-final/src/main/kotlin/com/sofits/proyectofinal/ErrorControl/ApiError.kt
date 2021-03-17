b3167bf92286326dec7a269aca6db6f4b5ea2a8d
ChristianPP15 <christianpayo32@gmail.com>
2021-03-17 19:00:16 +0100

    javadoc from error controll package

--- proyecto-final/src/main/kotlin/com/sofits/proyectofinal/ErrorControl/ApiError.kt
+++ proyecto-final/src/main/kotlin/com/sofits/proyectofinal/ErrorControl/ApiError.kt
@@ -5,20 +5,51 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import org.springframework.http.HttpStatus
 import java.time.LocalDateTime
 
+/**
+ * Data class que define el módelo de los mensajes de error
+ */
 data class ApiError(
+    /**
+     * Estado de respuesta HTTP
+     * @see HttpStatus
+     */
     val estado: HttpStatus,
+    /**
+     * Mensaje de error para el usuario
+     */
     val mensaje: String?,
-
+    /**
+     * Sub errores para dar más información al usuario
+     * @see ApiSubError
+     */
     @JsonInclude(JsonInclude.Include.NON_NULL)
     val subErrores: List<out ApiSubError>? = null,
-
+    /**
+     * Fecha y hora en la que se ha producido el fallo
+     */
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm:ss")
     val fecha: LocalDateTime = LocalDateTime.now(),
 )
+
+/**
+ * Clase abstractar para tratar los sub-errores
+ */
 open abstract class ApiSubError
 
+/**
+ * Data class que definen los sub-errores
+ */
 data class ApiValidationSubError(
+    /**
+     * Campo sobre el que se produce el fallo
+     */
     val campo : String,
+    /**
+     * Valor que se ha rechazado
+     */
     val valorRechazado : Any?,
+    /**
+     * Mensaje de error
+     */
     val mensaje : String?
 ) : ApiSubError()
--- proyecto-final/src/main/kotlin/com/sofits/proyectofinal/ErrorControl/Excepciones.kt
+++ proyecto-final/src/main/kotlin/com/sofits/proyectofinal/ErrorControl/Excepciones.kt
@@ -3,23 +3,54 @@ package com.sofits.proyectofinal.ErrorControl
 import com.sofits.proyectofinal.Modelos.UsuarioTieneLibroId
 import java.util.*
 
+/**
+ * Data class que define los errores generales extendiendo de RuntimeException
+ * @see RuntimeException
+ */
 open class EntityNotFoundExceptionControl(val msg: String) : RuntimeException(msg)
 
+/**
+ * Data class que define un mensaje de error para cuándo no encontramos un usuario por su id
+ */
 data class UserNotFoundById(val id: UUID) : EntityNotFoundExceptionControl("No se ha podido encontrar el usuario a partir de su ID")
 
+/**
+ * Data class que define si un error si un usuario ya existe, para ello comprobamos si el email ya existe
+ */
 data class UserAlreadyExit(val email:String) : EntityNotFoundExceptionControl("El nombre de usuario $email ya existe")
 
+/**
+ * Data class que define si el autor buscado por su id no existe
+ */
 data class AutorNotExist(val id:UUID) : EntityNotFoundExceptionControl("No existe un autor con id $id")
 
+/**
+ * Error para cuándo no se han encontrado autores
+ */
 data class AutorsNotExists(val mensaje: String="No existen autores aún") : EntityNotFoundExceptionControl(mensaje)
 
+/**
+ * Error para cuándo no se ha encontrado un libro por su id
+ */
 data class LibroNotExist(val id: UUID): EntityNotFoundExceptionControl("No existe un libro con id $id")
 
+/**
+ * Error para cuándo no se han encontrado libros
+ */
 data class LibrosNotExists(val mensaje: String="No existen libros aún") : EntityNotFoundExceptionControl(mensaje)
 
+/**
+ * Error para cuándo no se ha encontrado una publicación un el id proporcionado
+ */
 data class PublicacionNotExist(val id:UsuarioTieneLibroId): EntityNotFoundExceptionControl("No existen publicaciones para el id $id")
 
+/**
+ * Error para cuándo no existe una valoración de un usuario hacia el otro
+ */
 data class ValoracionNotExist(val idUsuarioValorado: UUID, val idUsuarioValorando:UUID)
     : EntityNotFoundExceptionControl("No existe valoración para el usuario con id $idUsuarioValorado y el $idUsuarioValorando")
 
-data class SearchProductoNoResultException(val mensaje: String ="No se encuentra productos con esas características"): EntityNotFoundExceptionControl(mensaje)
+/**
+ * Error para cuándo se busca un libro y no se encuentra libro con las características
+ */
+data class SearchLibroNoResultException(val mensaje: String ="No se encuentra productos con esas características"): EntityNotFoundExceptionControl(mensaje)
