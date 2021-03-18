f0e43be3c8a557b3273bff51db57be841bfea62b
ChristianPP15 <christianpayo32@gmail.com>
2021-03-18 23:21:22 +0100

    dagger add, but something was wrong, ccant connect to api

---
+++ proyecto-final-frontend/app/src/main/java/com/example/sofits_frontend/di/ApplicationComponent.kt
@@ -0,0 +1,17 @@
+package com.example.sofits_frontend.di
+
+import com.example.sofits_frontend.Api.NetworkModule
+import com.example.sofits_frontend.ui.Login.LoginActivity
+import com.example.sofits_frontend.ui.MiPerfil.MisLibrosFragment
+import com.example.sofits_frontend.ui.Registro.RegistroActivity
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [NetworkModule::class])
+interface ApplicationComponent {
+
+    fun inject(loginActivity: LoginActivity)
+    fun inject(registroActivity: RegistroActivity)
+    fun inject(misLibros:MisLibrosFragment)
+}
