3db4dac1f286076c3b729fe26a0bf22ed45cea69
ChristianPP15 <christianpayo32@gmail.com>
2021-03-19 12:19:27 +0100

    perfil completed

--- proyecto-final-frontend/app/src/main/java/com/example/sofits_frontend/ui/MiPerfil/Informacion/InformacionUsuarioFragment.kt
+++ proyecto-final-frontend/app/src/main/java/com/example/sofits_frontend/ui/MiPerfil/Informacion/InformacionUsuarioFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import coil.load
 import com.example.sofits_frontend.Api.Resource
@@ -13,6 +14,7 @@ import com.example.sofits_frontend.R
 import com.example.sofits_frontend.common.Constantes
 import com.example.sofits_frontend.common.MyApp
 import javax.inject.Inject
+import kotlin.math.roundToInt
 
 
 class InformacionUsuarioFragment : Fragment() {
@@ -31,7 +33,10 @@ class InformacionUsuarioFragment : Fragment() {
             when(response){
                 is Resource.Success->{
                     val imagenUsuario = view?.findViewById<ImageView>(R.id.imageView_perfilUsuarioImagen)
+                    val textViewNombreUsuario = view?.findViewById<TextView>(R.id.textView_nombreUsuario)
                     val usuario = response.data!!.usuarioValorado
+                    comprobarValoracion(response.data!!.media)
+                    textViewNombreUsuario?.text=usuario.nombre
                     if (usuario.imagen!=null){
                         imagenUsuario!!.load(Constantes.imageURL+usuario.imagen.idImagen)
                     }else{
@@ -41,6 +46,43 @@ class InformacionUsuarioFragment : Fragment() {
             }
         })
         return inflater.inflate(R.layout.fragment_informacion_usuario, container, false)
+    }
+
+    fun comprobarValoracion(valoracion:Double){
+        val estrella1= view?.findViewById<ImageView>(R.id.imageView_estrella1_valoracionPropia)
+        val estrella2= view?.findViewById<ImageView>(R.id.imageView_estrella2_valoracionPropia)
+        val estrella3= view?.findViewById<ImageView>(R.id.imageView_estrella3_valoracionPropia)
+        val estrella4= view?.findViewById<ImageView>(R.id.imageView_estrella4_valoracionPropia)
+        val estrella5= view?.findViewById<ImageView>(R.id.imageView_estrella5_valoracionPropia)
+        when(valoracion.roundToInt()){
+            1->{
+                estrella1?.setImageResource(R.drawable.ic_estrella_rellena)
+                estrella2?.setImageResource(R.drawable.ic_estrella_rellena)
+                estrella3?.setImageResource(R.drawable.ic_estrella_rellena)
+                estrella4?.setImageResource(R.drawable.ic_estrella_rellena)
+            }
+            2->{
+                estrella1?.setImageResource(R.drawable.ic_estrella_rellena)
+                estrella2?.setImageResource(R.drawable.ic_estrella_rellena)
+                estrella3?.setImageResource(R.drawable.ic_estrella_rellena)
+            }
+            3->{
+                estrella1?.setImageResource(R.drawable.ic_estrella_rellena)
+                estrella2?.setImageResource(R.drawable.ic_estrella_rellena)
+            }
+            4->{
+                estrella1?.setImageResource(R.drawable.ic_estrella_rellena)
+            }
+            0->{
+                estrella1?.setImageResource(R.drawable.ic_estrella_rellena)
+                estrella2?.setImageResource(R.drawable.ic_estrella_rellena)
+                estrella3?.setImageResource(R.drawable.ic_estrella_rellena)
+                estrella4?.setImageResource(R.drawable.ic_estrella_rellena)
+                estrella5?.setImageResource(R.drawable.ic_estrella_rellena)
+            }
+            else -> null
+        }
+
     }
 
 }
