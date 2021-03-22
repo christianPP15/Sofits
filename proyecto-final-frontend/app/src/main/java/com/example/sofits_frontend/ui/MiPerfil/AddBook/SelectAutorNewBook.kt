0f6ecd142c28f4094cf576471be11cdaf1aec86f
christian payo <christianpayo32@gmail.com>
2021-03-22 13:55:57 +0100

    fix spinner

--- proyecto-final-frontend/app/src/main/java/com/example/sofits_frontend/ui/MiPerfil/AddBook/SelectAutorNewBook.kt
+++ proyecto-final-frontend/app/src/main/java/com/example/sofits_frontend/ui/MiPerfil/AddBook/SelectAutorNewBook.kt
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.example.sofits_frontend.Api.Resource
 import com.example.sofits_frontend.Api.response.AutoresResponse.Autor
+import com.example.sofits_frontend.Api.response.AutoresResponse.Libro
 import com.example.sofits_frontend.R
 import com.example.sofits_frontend.common.MyApp
 import javax.inject.Inject
@@ -25,19 +26,21 @@ class SelectAutorNewBook : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         (this.applicationContext as MyApp).appComponent.inject(this)
         setContentView(R.layout.activity_select_autor_new_book)
-        spinner=findViewById(R.id.spinnerAutor)
+        spinner=findViewById(R.id.spinner_autor)
         addBookViewModel.cargarAutores()
         addBookViewModel.MyInfoData.observe(this, Observer {result ->
             when(result){
                 is Resource.Success->{
                     autores=result.data!!.content
-                    val listaNombres=result.data!!.content.map { it.nombre }
-                    val adapter: ArrayAdapter<String> = ArrayAdapter(this,R.layout.activity_select_autor_new_book,R.id.chooseAutor,listaNombres)
-                    spinner.adapter=adapter
+                    val listaNombres= mutableListOf<String>("Escoja un autor")
+                    for (nombre in result.data!!.content.map { it.nombre })
+                        listaNombres.add(nombre)
+                    spinner(listaNombres)
                 }
             }
         })
-        spinner.onItemSelectedListener = object : OnItemSelectedListener {
+
+        /*spinner.onItemSelectedListener = object : OnItemSelectedListener {
             override fun onItemSelected(parentView: AdapterView<*>?,selectedItemView: View?,position: Int,id: Long) {
                 val libros=autores.get(position).libros.map { it.titulo }
                 //val adapter: ArrayAdapter<String> = ArrayAdapter(applicationContext,R.layout.activity_select_autor_new_book,R.id.chooseLibro,libros)
@@ -49,7 +52,17 @@ class SelectAutorNewBook : AppCompatActivity() {
             override fun onNothingSelected(parentView: AdapterView<*>?) {
                 // your code here
             }
-        }
+        }*/
 
+    }
+    private fun spinner(lista:List<String>){
+        ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            lista
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
     }
 }
